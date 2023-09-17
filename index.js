c66078fd5cf4bd0cff0b25d40bda12a0b3a2c744
rushi <rushikeshthange2001@gmail.com>
2023-09-17 14:39:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const data=require("./data")
+app.use(cors())
+
+app.get("/data",(req, res)=>{
+    res.send(data)
+})
+app.listen("5000",()=>{
+    console.log("server is running 5000")
+})
